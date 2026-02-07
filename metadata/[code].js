fcc3576d7183559cc66f7e7d0662a6e210da552e
ahmadyani1st <151477384+ahmadyani1st@users.noreply.github.com>
2026-02-08 00:00:55 +0700

    Add API handler for fetching metadata by code

---
+++ metadata/[code].js
@@ -0,0 +1,30 @@
+// api/metadata/[code].js
+export default async function handler(req, res) {
+  const { code } = req.query;
+  
+  try {
+    const firebaseRes = await fetch(
+      `https://sahamuniversity-default-rtdb.asia-southeast1.firebasedatabase.app/shortUrls/${code}.json`
+    );
+
+    const data = await firebaseRes.json();
+    
+    res.json({
+      success: true,
+      data: {
+        targetUrl: data?.linkproduk,
+        metadata: {
+          title: data?.title,
+          description: data?.description,
+          image: data?.image
+        }
+      }
+    });
+    
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
+}
